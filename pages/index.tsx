5c2eddb4de9a98b70da51d9fbaa5ded2758cbb2a
Viet Anh Le <mc.vietanh@gmail.com>
2021-01-27 10:12:49 -0500

    hello world

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,29 @@
 import Link from 'next/link'
 import Layout from '../components/Layout';
 
+const students = [
+  'Aryel',
+  'David',
+  'Chirs',
+  'Colton',
+  'Hannah',
+  'Taha',
+]
+
+const StudentRender = () : any => {
+  let array =[];
+  for( let i = 0; i < students.length; i++){
+    array.push(<h3>{students[i]}</h3>)
+  }
+  return array;
+}
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello, Nafis Taha👋</h1>
-  
+    <h1>Hello, Interactive media design - Web Students👋</h1>
+
+    <StudentRender />
+    <h4>Sorry, I cannot know all of your names</h4>
     <p>
       <Link href="/about">
         <a>About</a>
