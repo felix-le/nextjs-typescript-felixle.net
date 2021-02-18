15384fe537f0b9f2ea712e427436bd5885f6e140
Viet Anh Le <mc.vietanh@gmail.com>
2021-02-18 05:16:35 -0500

    working on nav

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -12,34 +12,35 @@ const Layout: React.FunctionComponent<IProps> = ({
   children,
   title='this is the default title'
 }) => (
-  <>
+  <div>
     <Head>
-    <meta charSet="utf-8" />
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <title>{title}</title>
-    <meta
-        name="description"
-        content="I am a software engineer in Canada"
-      />
-      <meta property="og:locale" content="en_US" />
-      <meta property="og:type" content="website" />
-      <meta
-        property="og:title"
-        content="Felix Le - a Web developer - ReactJS"
-      />
-       <meta
-        property="og:description"
-        content=" I am a web developer who provide consultant and mobile and web development"
-      />
-      <meta property="og:url" content="https://www.linkedin.com/in/felix-le/" />
-      <meta property="og:site_name" content="Felix Le" />
-      <meta property="og:image" content="/assets/images/aboutMe/normal1.jpg" />
-      <meta property="og:image:width" content="1000" />
-      <meta property="og:image:height" content="961" />
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta 
+          name="description"
+          content="I am a software engineer in Canada"
+        />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Felix Le - a Web developer - ReactJS"
+        />
+        <meta
+          property="og:description"
+          content=" I am a web developer who provide consultant and mobile and web development"
+        />
+        <meta property="og:url" content="https://www.linkedin.com/in/felix-le/" />
+        <meta property="og:site_name" content="Felix Le" />
+        <meta property="og:image" content="/assets/images/aboutMe/VA1.jpg" />
+        <meta property="og:image:width" content="1000" />
+        <meta property="og:image:height" content="961" />
     </Head>
     <Header />
     {children}
     <Footer />
-  </>
+  </div>
 )
 export default Layout;
