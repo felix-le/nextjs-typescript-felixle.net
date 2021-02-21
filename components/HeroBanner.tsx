e0956138f79cc6f9ac7fa6f3717cbff03201c05a
Viet Anh Le <mc.vietanh@gmail.com>
2021-02-21 06:20:30 -0500

    updated text

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
-
-const HeroBanner: React.FunctionComponent = () =>(
+const HeroBanner: React.FunctionComponent = () => (
   <>
-    <div className="heroBanner-c">
-      <div className="container">
-        <div className="heroBanner__wrapper d-flex ">
-          <div className="heroBanner__text__wrapper">
-              <h1 className="heroBanner__tittle">Welcome to my house!</h1>
-              <p className="heroBanner__des">
-                <FontAwesomeIcon icon={faArrowRight} /> Hi, I'm Felix, a Web Developer and Lifelong learner
-              </p>
+    <div className='heroBanner-c'>
+      <div className='container'>
+        <div className='heroBanner__wrapper d-flex '>
+          <div className='heroBanner__text__wrapper'>
+            <h1 className='heroBanner__tittle'>Welcome to my house!</h1>
+            <p className='heroBanner__des'>
+              <FontAwesomeIcon icon={faArrowRight} /> I'm Felix,
+              <span className='text-success'> a Web Developer </span>
+              and
+              <span className='text-danger'> Lifelong learner</span>!
+            </p>
           </div>
-          <div className="heroBanner__image__wrapper">
-              <div className="overlay"></div>
-              <div className="heroBanner__bgAva"></div>
+          <div className='heroBanner__image__wrapper'>
+            <div className='overlay'></div>
+            <div className='heroBanner__bgAva'></div>
           </div>
         </div>
       </div>
     </div>
   </>
-)
+);
 export default HeroBanner;
