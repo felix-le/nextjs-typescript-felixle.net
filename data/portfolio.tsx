147750c83304d29a6dfa362e9089993a7ea61a47
Viet Anh Le <mc.vietanh@gmail.com>
2021-03-04 18:42:05 -0500

    updated iGEM task

--- data/portfolio.tsx
+++ data/portfolio.tsx
@@ -10,6 +10,7 @@ export interface IData {
   technology?: JSX.Element;
   numberImage?: number;
   images?: Array<string>;
+  clientLink?: string;
 }
 
 const data = [
@@ -97,10 +98,7 @@ const data = [
       <ul>
         <li>Date: 02/2020</li>
         <li>Site type: habits</li>
-        <li>
-          Owner: freeCodeCamp
-        </li>
-
+        <li>Owner: freeCodeCamp</li>
       </ul>
     ),
     technology: (
@@ -111,56 +109,109 @@ const data = [
     numberImage: 2,
   },
   {
-    image: 'felixle',
-    href: 'felixle.net',
-    title: 'FelixLe.net',
-    subtitle: 'Learning NextJS and TypeScript by projects.',
+    image: 'iGEM',
+    href: 'iGEM',
+    title: 'iGEM interview task',
+    subtitle: 'A Task Interview',
     description: (
       <>
-        <div>
+        <div className='mt--20'>
           <p>
-            Base on: RESTFUL Apis, I wanted to take its responses and reused
-            them cross the website. By doing that, I thought that it would be
-            easier for maintaining and controlling. <br />
+            Base on an SVG file to create a webpage where the SVG is animated
+            (var time) <br />
           </p>
-          <p>
-            For example: I had to build portfolio for Home page (slider),
-            portfolio page, and portfolio detail page. How can I use this their
-            data by a effective way?
-          </p>
-          <br />
-          <p>
-            Step 1: "Saw" my data.
-            <br />
-            Step 2: Tried to build a detail project based on data
-            <br />
-            Step 3: Create component portfolio <br />
-            Step 4: Input component portfolio into Homepage <br />
-          </p>
+          <div>
+            <p>
+              <strong>Requirements:</strong>
+            </p>
+            <ul>
+              <li>
+                Mario with a pipette represents our Wet Lab team: moving like
+                Mario
+              </li>
+              <li>
+                Yoshi with a laptop represents our Dry Lab team: moving like
+                Yoshi
+              </li>
+              <li>
+                Peach with a megaphone represents our Business and Ethics team:
+                moving like Peach
+              </li>
+              <li>
+                Plastics bottle represents our project on plastics degradation:
+                absolute position moving up and down
+              </li>
+              <li>
+                igemto logo pole represent our club: moving up and down while
+                attached to the bottom (scale x and y)
+              </li>
+              <li>
+                Bullet bill moving towards Mario, Yoshi, and Peach, who in
+                contrast will dodge the bullet.
+              </li>
+              <li>Bonus details: hand movement of the three characters</li>
+            </ul>
+          </div>
           <div>
             <p>
               <strong>What I did:</strong>
             </p>
             <ul>
-              <li>Used Data to cross all three pages in a dynamic way</li>
-              <li>Updated new detail page easy by using data</li>
+              <li>Used illustrator for design svg files</li>
+              <li>
+                Based on iGEM teams and their history to build a story about
+                them and their projects
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <p>
+              <strong>Result Links: </strong>
+            </p>
+            <ul>
+              <li>
+                <a
+                  href='https://uot-igem-club-interview.vercel.app/'
+                  style={{ color: 'red', fontSize: '18px' }}
+                >
+                  Demo
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://codepen.io/vietanh1808/pen/zYoLrwY'
+                  style={{ color: 'red', fontSize: '18px' }}
+                >
+                  codepen
+                </a>
+              </li>
+              <li>
+                <a
+                  href='https://github.com/felix-le/uot_igem_club_interview'
+                  style={{ color: 'red', fontSize: '18px' }}
+                >
+                  Github
+                </a>
+              </li>
             </ul>
           </div>
         </div>
       </>
     ),
+    clientLink: 'https://igemtoronto.ca/',
     aboutClient: (
       <ul>
-        <li>Date: 02/2020</li>
-        <li>Site type: Portfolio and Self-blog</li>
+        <li>Date: 03/2021</li>
+        <li>Site type: Small task</li>
         <li>Owner: Felix Le</li>
       </ul>
     ),
     technology: (
       <ul>
-        <li>TypeScript</li>
-        <li>NextJS</li>
-        <li>Scss</li>
+        <li>Pure JavaScript</li>
+        <li>Pure HTML</li>
+        <li>Pure CSS</li>
       </ul>
     ),
     numberImage: 1,
--- pages/portfolio/[detail].tsx
+++ pages/portfolio/[detail].tsx
@@ -9,28 +9,28 @@ import data from '../../data/portfolio';
 const PortfolioDetailPage: NextPage = () => {
   const router = useRouter();
   const { detail } = router.query;
-  const infoDetail = data.find(item => item.href === detail);
+  const infoDetail = data.find((item) => item.href === detail);
   const morePortfolio = data
-    .filter(item => item.href !== detail)
-    .sort(function() {
+    .filter((item) => item.href !== detail)
+    .sort(function () {
       return 0.5 - Math.random();
     })
     .slice(0, 2);
 
   return (
-    <Layout title="Portfolio Detail Page">
+    <Layout title='Portfolio Detail Page'>
       {infoDetail && (
         <>
           {/* Start Breadcrump Area */}
           <div
-            className="rn-page-title-area pt--120 pb--190 bg_image bg_image--4"
-            data-black-overlay="7"
+            className='rn-page-title-area pt--120 pb--190 bg_image bg_image--4'
+            data-black-overlay='7'
           >
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-12">
-                  <div className="rn-page-title text-center pt--100">
-                    <h2 className="title theme-gradient">
+            <div className='container'>
+              <div className='row'>
+                <div className='col-lg-12'>
+                  <div className='rn-page-title text-center pt--100'>
+                    <h2 className='title theme-gradient'>
                       {infoDetail?.title}
                     </h2>
                     <p>{infoDetail?.subtitle}</p>
@@ -41,50 +41,61 @@ const PortfolioDetailPage: NextPage = () => {
           </div>
           {/* End Breadcrump Area */}
 
-
           {/* Start Portfolio Details */}
-          <div className="rn-portfolio-details ptb--120 bg_color--1">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-12">
-                  <div className="portfolio-details">
-                    <div className="inner">
+          <div className='rn-portfolio-details ptb--120 bg_color--1'>
+            <div className='container'>
+              <div className='row'>
+                <div className='col-lg-12'>
+                  <div className='portfolio-details'>
+                    <div className='inner'>
                       <h2>{infoDetail?.title}</h2>
-                      <p className="subtitle">{infoDetail?.subtitle}</p>
+                      <p className='subtitle' style={{ marginBottom: 0 }}>
+                        <strong>{infoDetail?.subtitle}</strong>
+                      </p>
+                      {infoDetail?.clientLink && (
+                        <a
+                          href='infoDetail?.clientLink '
+                          target='_blank'
+                          style={{ marginBottom: '10px' }}
+                        >
+                          {infoDetail?.clientLink}
+                        </a>
+                      )}
                       {infoDetail?.description}
-                      <div className="portfolio-view-list d-flex flex-wrap">
+                      <div className='portfolio-view-list d-flex flex-wrap'>
                         {infoDetail?.aboutClient && (
-                          <div className="port-view">
-                            <h4>About Client</h4>
+                          <div className='port-view'>
+                            <h4>About the project/task</h4>
                             {infoDetail?.aboutClient}
                           </div>
                         )}
                         {infoDetail?.technology && (
-                          <div className="port-view">
+                          <div className='port-view'>
                             <h4>Technology stack</h4>
                             {infoDetail?.technology}
                           </div>
                         )}
                       </div>
                     </div>
-                    <div className="portfolio-thumb-inner">
-                      {infoDetail?.numberImage && Array.from(Array(infoDetail?.numberImage).keys()).map(
-                        index => (
-                          <div
-                            className={`thumb mb--30 ${
-                              index === 0 ? 'position-relative ' : ''
-                            }`}
-                            key={index}
-                          >
-                            <img
-                              src={`/assets/images/portfolio/${
-                                infoDetail?.image
-                              }/${index + 1}.png`}
-                              alt="Portfolio Images"
-                            />
-                          </div>
-                        )
-                      )}
+                    <div className='portfolio-thumb-inner'>
+                      {infoDetail?.numberImage &&
+                        Array.from(Array(infoDetail?.numberImage).keys()).map(
+                          (index) => (
+                            <div
+                              className={`thumb mb--30 ${
+                                index === 0 ? 'position-relative ' : ''
+                              }`}
+                              key={index}
+                            >
+                              <img
+                                src={`/assets/images/portfolio/${
+                                  infoDetail?.image
+                                }/${index + 1}.png`}
+                                alt='Portfolio Images'
+                              />
+                            </div>
+                          )
+                        )}
                     </div>
                   </div>
                 </div>
@@ -94,25 +105,25 @@ const PortfolioDetailPage: NextPage = () => {
           {/* End Portfolio Details */}
 
           {/* Start Related Work */}
-          <div className="portfolio-related-work pb--120 bg_color--1">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-12">
-                  <div className="section-title text-center">
-                    <span className="theme-color font--18 fontWeight600">
+          <div className='portfolio-related-work pb--120 bg_color--1'>
+            <div className='container'>
+              <div className='row'>
+                <div className='col-lg-12'>
+                  <div className='section-title text-center'>
+                    <span className='theme-color font--18 fontWeight600'>
                       Related Work
                     </span>
                     <h2>Our More Projects</h2>
                   </div>
                 </div>
               </div>
-              <div className="row mt--10">
+              <div className='row mt--10'>
                 {morePortfolio.map((item, index) => (
-                  <div className="col-lg-6 col-md-6 col-12" key={index}>
-                    <div className="related-work text-center mt--30">
-                      <div className="thumb">
+                  <div className='col-lg-6 col-md-6 col-12' key={index}>
+                    <div className='related-work text-center mt--30'>
+                      <div className='thumb'>
                         <Link
-                          href="/portfolio/[detail]"
+                          href='/portfolio/[detail]'
                           as={`/portfolio/${item.href}`}
                         >
                           <a href={`/portfolio/${item.href}`}>
@@ -123,10 +134,10 @@ const PortfolioDetailPage: NextPage = () => {
                           </a>
                         </Link>
                       </div>
-                      <div className="inner">
+                      <div className='inner'>
                         <h4>
                           <Link
-                            href="/portfolio/[detail]"
+                            href='/portfolio/[detail]'
                             as={`/portfolio/${item.href}`}
                           >
                             <a href={`/portfolio/${item.href}`}>{item.title}</a>
