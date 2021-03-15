cb6ec333dbac3e40569b65bb5585a552b3335b5c
Viet Anh Le <mc.vietanh@gmail.com>
2021-03-15 09:46:24 -0400

    fix footer

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -15,9 +15,9 @@ const Footer: React.FunctionComponent = () => (
   >
     <div className='wrapper plr--50 plr_sm--20'>
       <div className='row align-items-center justify-content-between'>
-        <div className='col-lg-6 col-md-12 col-sm-6 col-12'>
-          <div className='inner text-center'>
-            <ul className='social-share rn-lg-size d-flex justify-content-center text-center liststyle'>
+        <div className='col-lg-6 col-md-12 col-sm-12 col-12'>
+          <div className='inner '>
+            <ul className='social-share rn-lg-size d-flex liststyle'>
               {SocialShare.map((val, i) => (
                 <li key={i}>
                   <a href={`${val.link}`}>{val.Social}</a>
