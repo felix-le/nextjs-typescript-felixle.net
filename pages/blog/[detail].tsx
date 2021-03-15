fbcacf81430ad5e61fec40a18319b32d84ee3f00
Viet Anh Le <mc.vietanh@gmail.com>
2021-03-14 23:51:22 -0400

    done detail page

--- pages/blog/[detail].tsx
+++ pages/blog/[detail].tsx
@@ -19,134 +19,143 @@ const LearningDetailPage: NextPage = () => {
 
   return (
     <Layout title='Learning Detail Page'>
-      {infoDetail && (
-        <>
-          {/* Start Breadcrump Area */}
-          <div
-            className='rn-page-title-area pt--120 pb--190 bg_image bg_image--4'
-            data-black-overlay='7'
-          >
-            <div className='container'>
-              <div className='row'>
-                <div className='col-lg-12'>
-                  <div className='rn-page-title text-center pt--100'>
-                    <h2 className='title theme-gradient'>
-                      {infoDetail?.title}
-                    </h2>
-                    <p>{infoDetail?.subtitle}</p>
+      <div className='blog_detailPage'>
+        {infoDetail && (
+          <>
+            {/* Start Breadcrump Area */}
+            <div
+              className='rn-page-title-area pt--120 pb--190 bg_image bg_image--5'
+              data-black-overlay='7'
+            >
+              <div className='container'>
+                <div className='row'>
+                  <div className='col-lg-12'>
+                    <div className='rn-page-title text-center pt--100'>
+                      <h2 className='title theme-gradient'>
+                        {infoDetail?.title}
+                      </h2>
+                      <p>{infoDetail?.subtitle}</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          {/* End Breadcrump Area */}
+            {/* End Breadcrump Area */}
 
-          {/* Start Portfolio Details */}
-          <div className='rn-portfolio-details ptb--120 bg_color--1'>
-            <div className='container'>
-              <div className='row'>
-                <div className='col-lg-12'>
-                  <div className='portfolio-details'>
-                    <div className='inner'>
-                      <h2>{infoDetail?.title}</h2>
-                      <p className='subtitle' style={{ marginBottom: 0 }}>
-                        <strong>{infoDetail?.subtitle}</strong>
-                      </p>
-                      {infoDetail?.courseLink && (
-                        <a
-                          href={infoDetail.courseLink}
-                          target='_blank'
-                          style={{ marginBottom: '10px' }}
-                        >
-                          {infoDetail?.courseLink}
-                        </a>
-                      )}
-                      {infoDetail?.description}
-                      <div className='portfolio-view-list d-flex flex-wrap'>
-                        {/* {infoDetail?.aboutClient && (
-                          <div className='port-view'>
-                            <h4>About the project/task</h4>
-                            {infoDetail?.aboutClient}
-                          </div>
-                        )} */}
-                        {infoDetail?.technology && (
-                          <div className='port-view'>
-                            <h4>Technology stack</h4>
-                            {infoDetail?.technology}
-                          </div>
+            {/* Start Portfolio Details */}
+            <div className='rn-portfolio-details ptb--120 bg_color--3'>
+              <div className='container'>
+                <div className='row'>
+                  <div className='col-lg-12'>
+                    <div className='portfolio-details'>
+                      <div className='inner'>
+                        <h2>{infoDetail?.title}</h2>
+                        <p className='subtitle' style={{ marginBottom: 0 }}>
+                          {infoDetail?.description}
+                        </p>
+                        {infoDetail?.courseLink && (
+                          <a
+                            href={infoDetail.courseLink}
+                            target='_blank'
+                            style={{ marginBottom: '10px' }}
+                            className='link_to_program'
+                          >
+                            {infoDetail?.program}
+                          </a>
                         )}
+                        <br />
                       </div>
-                    </div>
-                    <div className='portfolio-thumb-inner'>
-                      {infoDetail?.numberImage &&
-                        Array.from(Array(infoDetail?.numberImage).keys()).map(
-                          (index) => (
-                            <div
-                              className={`thumb mb--30 ${
-                                index === 0 ? 'position-relative ' : ''
-                              }`}
-                              key={index}
-                            >
+                      {infoDetail?.numberContentImage && (
+                        <div className='contentImage__wrapper'>
+                          {Array.from(
+                            Array(infoDetail?.numberContentImage).keys()
+                          ).map((index) => (
+                            <div className='col-md-12 col-lg-6' key={index}>
                               <img
                                 src={`/assets/images/blog/${
-                                  infoDetail?.image
+                                  infoDetail?.contentImgLink
                                 }/${index + 1}.png`}
-                                alt='Portfolio Images'
+                                alt='Blog content Images'
                               />
                             </div>
-                          )
-                        )}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* End Portfolio Details */}
+                          ))}
+                        </div>
+                      )}
 
-          {/* Start Related Work */}
-          <div className='portfolio-related-work pb--120 bg_color--1'>
-            <div className='container'>
-              <div className='row'>
-                <div className='col-lg-12'>
-                  <div className='section-title text-center'>
-                    <span className='theme-color font--18 fontWeight600'>
-                      Related Work
-                    </span>
-                    <h2>Our More Projects</h2>
-                  </div>
-                </div>
-              </div>
-              <div className='row mt--10'>
-                {moreLearning.map((item, index) => (
-                  <div className='col-lg-6 col-md-6 col-12' key={index}>
-                    <div className='related-work text-center mt--30'>
-                      <div className='thumb'>
-                        <Link href='/blog/[detail]' as={`/blog/${item.href}`}>
-                          <a href={`/blog/${item.href}`}>
-                            <img
-                              src={`/assets/images/blog/${item.image}/1.png`}
-                              alt={item.title}
-                            />
-                          </a>
-                        </Link>
-                      </div>
-                      <div className='inner'>
-                        <h4>
-                          <Link href='/blog/[detail]' as={`/blog/${item.href}`}>
-                            <a href={`/blog/${item.href}`}>{item.title}</a>
-                          </Link>
-                        </h4>
+                      <div className='portfolio-thumb-inner mt--30'>
+                        {infoDetail?.numberImage &&
+                          Array.from(Array(infoDetail?.numberImage).keys()).map(
+                            (index) => (
+                              <div
+                                className={`thumb mb--30 ${
+                                  index === 0 ? 'position-relative ' : ''
+                                }`}
+                                key={index}
+                              >
+                                <img
+                                  src={`/assets/images/blog/${
+                                    infoDetail?.image
+                                  }/${index + 1}.png`}
+                                  alt='Blog Images'
+                                />
+                              </div>
+                            )
+                          )}
                       </div>
                     </div>
                   </div>
-                ))}
+                </div>
               </div>
             </div>
-          </div>
-          {/* End Related Work */}
-        </>
-      )}
+            {/* End Portfolio Details */}
+
+            {/* Start Related Work */}
+            <div className='portfolio-related-work pb--120 bg_color--1'>
+              <div className='container'>
+                <div className='row'>
+                  <div className='col-lg-12'>
+                    <div className='section-title text-center'>
+                      <span className='theme-color font--18 fontWeight600'>
+                        Related Work
+                      </span>
+                      <h2>Our More Projects</h2>
+                    </div>
+                  </div>
+                </div>
+                <div className='row mt--10'>
+                  {moreLearning.map((item, index) => (
+                    <div className='col-lg-6 col-md-6 col-12' key={index}>
+                      <div className='related-work text-center mt--30'>
+                        <div className='thumb'>
+                          <Link href='/blog/[detail]' as={`/blog/${item.href}`}>
+                            <a href={`/blog/${item.href}`}>
+                              <img
+                                src={`/assets/images/blog/${item.image}/1.png`}
+                                alt={item.title}
+                              />
+                            </a>
+                          </Link>
+                        </div>
+                        <div className='inner'>
+                          <h4>
+                            <Link
+                              href='/blog/[detail]'
+                              as={`/blog/${item.href}`}
+                            >
+                              <a href={`/blog/${item.href}`}>{item.title}</a>
+                            </Link>
+                          </h4>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            {/* End Related Work */}
+          </>
+        )}
+      </div>
     </Layout>
   );
 };
