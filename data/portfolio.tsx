a6a89efd884ff8112bc0c3a6a2fc61cbafc40519
Viet Anh Le <mc.vietanh@gmail.com>
2021-03-17 00:43:59 -0400

    updated

--- data/portfolio.tsx
+++ data/portfolio.tsx
@@ -108,114 +108,114 @@ const data = [
     ),
     numberImage: 2,
   },
-  {
-    image: 'iGEM',
-    href: 'iGEM',
-    title: 'iGEM interview task',
-    subtitle: 'A Task Interview',
-    description: (
-      <>
-        <div className='mt--20'>
-          <p>
-            Base on an SVG file to create a webpage where the SVG is animated
-            (var time) <br />
-          </p>
-          <div>
-            <p>
-              <strong>Requirements:</strong>
-            </p>
-            <ul>
-              <li>
-                Mario with a pipette represents our Wet Lab team: moving like
-                Mario
-              </li>
-              <li>
-                Yoshi with a laptop represents our Dry Lab team: moving like
-                Yoshi
-              </li>
-              <li>
-                Peach with a megaphone represents our Business and Ethics team:
-                moving like Peach
-              </li>
-              <li>
-                Plastics bottle represents our project on plastics degradation:
-                absolute position moving up and down
-              </li>
-              <li>
-                igemto logo pole represent our club: moving up and down while
-                attached to the bottom (scale x and y)
-              </li>
-              <li>
-                Bullet bill moving towards Mario, Yoshi, and Peach, who in
-                contrast will dodge the bullet.
-              </li>
-              <li>Bonus details: hand movement of the three characters</li>
-            </ul>
-          </div>
-          <div>
-            <p>
-              <strong>What I did:</strong>
-            </p>
-            <ul>
-              <li>Used illustrator for design svg files</li>
-              <li>
-                Based on iGEM teams and their history to build a story about
-                them and their projects
-              </li>
-            </ul>
-          </div>
+  // {
+  //   image: 'iGEM',
+  //   href: 'iGEM',
+  //   title: 'iGEM interview task',
+  //   subtitle: 'A Task Interview',
+  //   description: (
+  //     <>
+  //       <div className='mt--20'>
+  //         <p>
+  //           Base on an SVG file to create a webpage where the SVG is animated
+  //           (var time) <br />
+  //         </p>
+  //         <div>
+  //           <p>
+  //             <strong>Requirements:</strong>
+  //           </p>
+  //           <ul>
+  //             <li>
+  //               Mario with a pipette represents our Wet Lab team: moving like
+  //               Mario
+  //             </li>
+  //             <li>
+  //               Yoshi with a laptop represents our Dry Lab team: moving like
+  //               Yoshi
+  //             </li>
+  //             <li>
+  //               Peach with a megaphone represents our Business and Ethics team:
+  //               moving like Peach
+  //             </li>
+  //             <li>
+  //               Plastics bottle represents our project on plastics degradation:
+  //               absolute position moving up and down
+  //             </li>
+  //             <li>
+  //               igemto logo pole represent our club: moving up and down while
+  //               attached to the bottom (scale x and y)
+  //             </li>
+  //             <li>
+  //               Bullet bill moving towards Mario, Yoshi, and Peach, who in
+  //               contrast will dodge the bullet.
+  //             </li>
+  //             <li>Bonus details: hand movement of the three characters</li>
+  //           </ul>
+  //         </div>
+  //         <div>
+  //           <p>
+  //             <strong>What I did:</strong>
+  //           </p>
+  //           <ul>
+  //             <li>Used illustrator for design svg files</li>
+  //             <li>
+  //               Based on iGEM teams and their history to build a story about
+  //               them and their projects
+  //             </li>
+  //           </ul>
+  //         </div>
 
-          <div>
-            <p>
-              <strong>Result Links: </strong>
-            </p>
-            <ul>
-              <li>
-                <a
-                  href='https://uot-igem-club-interview.vercel.app/'
-                  style={{ color: 'red', fontSize: '18px' }}
-                >
-                  Demo
-                </a>
-              </li>
-              <li>
-                <a
-                  href='https://codepen.io/vietanh1808/pen/zYoLrwY'
-                  style={{ color: 'red', fontSize: '18px' }}
-                >
-                  codepen
-                </a>
-              </li>
-              <li>
-                <a
-                  href='https://github.com/felix-le/uot_igem_club_interview'
-                  style={{ color: 'red', fontSize: '18px' }}
-                >
-                  Github
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </>
-    ),
-    clientLink: 'https://igemtoronto.ca/',
-    aboutClient: (
-      <ul>
-        <li>Date: 03/2021</li>
-        <li>Site type: Small task</li>
-        <li>Owner: Felix Le</li>
-      </ul>
-    ),
-    technology: (
-      <ul>
-        <li>Pure JavaScript</li>
-        <li>Pure HTML</li>
-        <li>Pure CSS</li>
-      </ul>
-    ),
-    numberImage: 2,
-  },
+  //         <div>
+  //           <p>
+  //             <strong>Result Links: </strong>
+  //           </p>
+  //           <ul>
+  //             <li>
+  //               <a
+  //                 href='https://uot-igem-club-interview.vercel.app/'
+  //                 style={{ color: 'red', fontSize: '18px' }}
+  //               >
+  //                 Demo
+  //               </a>
+  //             </li>
+  //             <li>
+  //               <a
+  //                 href='https://codepen.io/vietanh1808/pen/zYoLrwY'
+  //                 style={{ color: 'red', fontSize: '18px' }}
+  //               >
+  //                 codepen
+  //               </a>
+  //             </li>
+  //             <li>
+  //               <a
+  //                 href='https://github.com/felix-le/uot_igem_club_interview'
+  //                 style={{ color: 'red', fontSize: '18px' }}
+  //               >
+  //                 Github
+  //               </a>
+  //             </li>
+  //           </ul>
+  //         </div>
+  //       </div>
+  //     </>
+  //   ),
+  //   clientLink: 'https://igemtoronto.ca/',
+  //   aboutClient: (
+  //     <ul>
+  //       <li>Date: 03/2021</li>
+  //       <li>Site type: Small task</li>
+  //       <li>Owner: Felix Le</li>
+  //     </ul>
+  //   ),
+  //   technology: (
+  //     <ul>
+  //       <li>Pure JavaScript</li>
+  //       <li>Pure HTML</li>
+  //       <li>Pure CSS</li>
+  //     </ul>
+  //   ),
+  //   numberImage: 2,
+  // },
   {
     image: 'academicPHP',
     href: 'academicPHP',
