6d3e4e64d24e8ff0f402f82ae8630b660e39ac12
Viet Anh Le <mc.vietanh@gmail.com>
2022-10-31 16:16:30 -0400

    update title

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -5,46 +5,43 @@ import Header from './Header';
 import Footer from './Footer';
 
 type IProps = {
-  title?:string;
+  title?: string;
 };
 
 const Layout: React.FunctionComponent<IProps> = ({
   children,
-  title='this is the default title'
+  title = 'this is the default title',
 }) => (
   <div>
     <Head>
       <title>{title}</title>
-      <meta charSet="utf-8" />
-      
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta 
-          name="description"
-          content="I am a software engineer in Canada"
-        />
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:title"
-          content="Felix Le - a Web developer - ReactJS"
-        />
-        <meta
-          property="og:description"
-          content=" I am a web developer who provide consultant and mobile and web development"
-        />
-        <meta name="google-site-verification" content="XMsTDnI6zOJIY2bfKtGGZe9eJG3WujClHrzBUrtwBJA" />
-        <meta property="og:url" content="https://www.linkedin.com/in/felix-le/" />
-        <meta property="og:site_name" content="Felix Le" />
-        <meta property="og:image" content="/assets/images/about/url_img.png" />
-        <meta property="og:image:width" content="500" />
-        <meta property="og:image:height" content="500" />
-        <script
+      <meta charSet='utf-8' />
+
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <meta name='description' content='I am a software engineer in Canada' />
+      <meta property='og:locale' content='en_US' />
+      <meta property='og:type' content='website' />
+      <meta property='og:title' content='Felix Le - a Full-Stack - ReactJS' />
+      <meta
+        property='og:description'
+        content=' I am a Full-Stack who provide consultant and mobile and web development'
+      />
+      <meta
+        name='google-site-verification'
+        content='XMsTDnI6zOJIY2bfKtGGZe9eJG3WujClHrzBUrtwBJA'
+      />
+      <meta property='og:url' content='https://www.linkedin.com/in/felix-le/' />
+      <meta property='og:site_name' content='Felix Le' />
+      <meta property='og:image' content='/assets/images/about/url_img.png' />
+      <meta property='og:image:width' content='500' />
+      <meta property='og:image:height' content='500' />
+      <script
         dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NJH9SVV');`
+          })(window,document,'script','dataLayer','GTM-NJH9SVV');`,
         }}
       ></script>
     </Head>
@@ -52,5 +49,5 @@ const Layout: React.FunctionComponent<IProps> = ({
     {children}
     <Footer />
   </div>
-)
+);
 export default Layout;
